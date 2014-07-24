1dc774d7010907748b67ec95e2ae879947d3d0e0
stefceror <stefsnow@gmail.com>
2014-07-24 15:26:34 -0700

    Prototypal and pseudoclassical instantiations passes specs.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var obj = Object.create(stackMethods);
+  obj.currentSize = 0;
+  return obj;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this[this.currentSize] = value;
+  this.currentSize++;
+};
+
+stackMethods.pop = function() {
+  this.currentSize && this.currentSize--;
+  var result = this[this.currentSize];
+  delete this[this.currentSize];
+  return result;
+};
+
+stackMethods.size = function() {
+  return this.currentSize;
+};
 
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.firstElement = 0;
+  this.lastElement = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this[this.lastElement] = value;
+  this.lastElement++;
+};
 
+Queue.prototype.dequeue = function() {
+  var result = this[this.firstElement];
+  delete this[this.firstElement];
+  this.firstElement++;
+  return result;
+};
+
+Queue.prototype.size = function() {
+  if (this.lastElement >= this.firstElement) {
+    return this.lastElement - this.firstElement;
+  }
+  return 0;
+};
