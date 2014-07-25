64c678d82d7f39b83c14e5e56aebaf8890b41576
stefceror <stefsnow@gmail.com>
2014-07-24 21:03:02 -0700

    Tree and set data structures pass specs

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,14 +7,29 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if(this._storage === undefined) {
+    this._storage = [];
+  }
+  if(!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item){
+  return _.contains(this._storage, item);
 };
 
 setPrototype.remove = function(item){
+  for(var i = 0; i < this._storage.length; i++) {
+    if(this._storage[i] === item) {
+      this._storage.splice(i, 1);
+    }
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * add: O(1)
+ * contains: O(n) because one for loop exists in _.contains
+ * remove: O(n)
  */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,6 +2,7 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -11,14 +12,31 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  if(this.children===undefined){
+    this.children=[];
+  }
+  this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
-
+  var targetFound = false;
+  if(this.value===target){
+    targetFound = true;
+  }
+  if(this.children){
+    for(var i=0; i< this.children.length; i++){
+      targetFound = this.children[i].contains(target);
+      if(targetFound){
+        break;
+      }
+    }
+  }
+  return targetFound;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addChild: O(1)
+ * contains: O(n) because each tree is only checked once
  */
