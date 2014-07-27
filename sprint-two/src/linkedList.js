ea98b5ab275579662b445a4bf14f2dc0149f3bb9
Stefanie Contreras <stefsnow@gmail.com>
2014-07-27 10:55:06 -0700

    linked list passes tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,46 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    //create new node at tail
+    var newNode = makeNode(value);
+    if(this.tail !== null){
+      this.tail.next = newNode;
+    }
+    this.tail = newNode;
+    //if head null, set to node
+    if(this.head === null){
+      this.head = newNode;
+    }
   };
 
   list.removeHead = function(){
+    //save old head;
+    var oldHead= this.head;
+    //set head to next item in list if it exists
+    if(oldHead.next){
+      this.head = oldHead.next;
+    //if not, set list to be empty
+    } else{
+      this.head = null;
+      this.tail = null;
+    }
+    return oldHead.value; 
   };
 
   list.contains = function(target){
+    //start at head
+    var currNode = this.head;
+    //check each node that exists
+    while(currNode){
+      if(currNode.value === target){
+        //if target return true
+        return true;
+      //if not found return false
+      } else {
+        currNode = currNode.next;
+      }
+    }
+    return false;
   };
 
   return list;
